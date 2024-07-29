6af106ade8c4b6263941b384ac162f0c46b3fb8b
Umehabiba982 <umehabibanazeer@gmail.com>
2024-07-29 16:24:53 +0500

    Add files via upload

---
+++ operator.js
@@ -0,0 +1,76 @@
+
+// document.write("Operator");
+
+// let a1 = parseInt(prompt("1"))
+// let a2 = parseInt(prompt("2"))
+
+// let result = a1+a2;
+// alert(result);
+
+// let NamE = "Ramsha";
+// var Name = "soha";
+// const gender = "female";
+
+// console.log(namE);
+// console.log(Name);
+
+let   first = 4;
+let   Second = 2;
+let   third = "8";
+
+
+let result1 = first-Second;
+let result2 = first/Second;
+let result3 = first*Second;
+let result4 = first**Second;
+let result5 = first%Second;
+let result6 = first++;
+let result7 = first--;
+let result8 = --first;
+let result9 = Second--;
+let result10 = Second++;
+
+
+// document.write("Result = " + result1+ "<br>");
+// document.write("Result = " + result2+ "<br>");
+// document.write("Result = " + result3+ "<br>");
+// document.write("Result = " + result4+ "<br>");
+// document.write("Result = " + result5+ "<br>");
+document.write("Result = " + result6+ "<br>");
+document.write("Result = " + result7+ "<br>");
+document.write("Result = " + result8+ "<br>");
+document.write("Result = " + result9+ "<br>");
+document.write("Result = " + result10+ "<br>");
+
+// let aa = "8";
+// let a = 8;
+// let b = 2;
+// let c = a > b;
+// let c1 = a <= b;
+// let c3 = a==aa;
+// let c4 = a===aa;
+
+// // document.write("Result = " + c+ "<br>");
+// // document.write("Result = " + c1+ "<br>");
+// // document.write("Result = " + c3+ "<br>");
+// document.write("Result = " + c4+ "<br>");
+
+
+
+
+// let isSunny = true;
+// let isWarm = true;
+// if (isSunny && isWarm) {
+//     // Code to execute when both conditions are true
+// }
+
+// let hasCoffee = true;
+// let hasTea = false;
+// if (hasCoffee || hasTea) {
+//     // Code to execute if either condition is true
+// }
+
+// let isRaining = false;
+// if (!isRaining) {
+//     // Code to execute if it's not raining
+// }
